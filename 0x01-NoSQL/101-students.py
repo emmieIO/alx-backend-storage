ffb31af77256417d96fbd15376a722d33c188510
Onuoha nnaemeka <markonuoha6@gmail.com>
2024-05-15 17:51:33 +0100

    12-log_stats

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,39 +1,27 @@
 #!/usr/bin/env python3
-"""101-students.py"""
-from pymongo import AggregationPipeline
+"""Task 14"""
 
 
 def top_students(mongo_collection):
+    """Prints all students in a collection sorted by average score.
     """
-        Returns all students from
-        the collection sorted by average score.
-
-    Args:
-        mongo_collection: A pymongo collection object.
-
-    Returns:
-        A cursor object containing
-        documents with student information and
-        a new key "averageScore" for the
-        average score.
-    """
-    pipeline = [
-        {
-            '$project': {
-                '_id': 1,
-                'name': 1,
-                'averageScore': {
-                    '$avg': {
-                    '$avg': '$topics.score',
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
                     },
+                    'topics': 1,
                 },
-                'topics': 1,
             },
-        },
-        {
-            '$sort': {'averageScore': -1},
-        },
-    ]
-
-    aggregation = AggregationPipeline(pipeline)
-    return mongo_collection.aggregate(aggregation)
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
